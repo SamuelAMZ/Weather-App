df471029dc72124807de76b75f0e3247b1a3c11f
sam_dev <samueldev09@gmail.com>
2022-07-27 10:56:54 +0200

    2/3 of  pages added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,1 @@
-import { DashboardEvents } from "./assets/ts/index";
 import "./assets/scss/style.scss";
-
-const AllEvents = new DashboardEvents();
-
-// ---------------- events for the dashboard controls ----------------
-window.addEventListener("DOMContentLoaded", (): void => {
-  // dashboard events
-  AllEvents.dashboardEvent();
-  AllEvents.themeEvent();
-  // app events
-});
